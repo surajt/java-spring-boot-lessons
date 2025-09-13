d145964c40feded4fb032216510bfffb082c7c6d
SurajT <>
2025-09-13 13:45:13 -0400

    Test push

--- src/main/java/com/lessons/Main.java
+++ src/main/java/com/lessons/Main.java
@@ -3,6 +3,6 @@ package com.lessons;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Welcome to Java Spring Lessons.");
+        System.out.printf("Welcome to Java Spring Lessons .");
     }
 }
